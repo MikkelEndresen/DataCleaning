c945d41fa32033f8c5b34540d2c999fad1bff4a3
Mickey <72854733+MikkelEndresen@users.noreply.github.com>
2024-03-30 11:49:41 +1100

    Succesful for example data

---
+++ dataCleaning/myapi/infer_data_types.py
@@ -0,0 +1,87 @@
+import pandas as pd
+from llm_api_client import llm_to_dtype
+
+def infer_and_convert_data_types(df):
+    """
+    Covert df to appropriate data types
+
+    Parameters:
+        - df
+    
+    Returns: 
+        - df
+    """
+
+    # Check for header
+    if isinstance(df.columns[0], str):
+        print(df.head(3))
+        # if header give it to gemini and convert columns to appropriate datatypes
+        dtypes = llm_to_dtype(df.head(3))
+
+        print(f'dtypes are: {dtypes}')
+
+        # Test
+        print(df.dtypes)
+        df = str_col_to_dtype(dtypes, df)
+        print(df.dtypes)
+
+        return None
+
+    for col in df.columns:
+        print(df[col])
+
+        # Attempt to convert to numeric first
+        df_converted = pd.to_numeric(df[col], errors='coerce')
+        if not df_converted.isna().all():  # If at least one value is numeric
+            df[col] = df_converted
+            continue
+
+        # Attempt to convert to datetime
+        try:
+            df[col] = pd.to_datetime(df[col])
+            continue
+        except (ValueError, TypeError):
+            pass
+
+        # Check if the column should be categorical
+        if len(df[col].unique()) / len(df[col]) < 0.5:  # Example threshold for categorization
+            df[col] = pd.Categorical(df[col])
+
+    return df
+
+def str_col_to_dtype(dtypes, df):
+    """
+    
+    Return:
+        - Column with correct dtype
+    """
+    for i, col in enumerate(df.columns):
+        if dtypes[i][:3] == 'int':
+            df[col] = pd.to_numeric(df[col], downcast='unsigned', errors='coerce')
+            df[col]= df[col].convert_dtypes(convert_integer = True)
+            #df[col] = df[col].astype('int32', errors='raise')
+        elif dtypes[i] == 'datetime64':
+            try:
+                df[col] = pd.to_datetime(df[col])
+                continue
+            except (ValueError, TypeError):
+                pass
+        elif dtypes[i] == 'category':
+            print('enters')
+            df[col] = pd.Categorical(df[col])
+                
+        
+    print(df)
+    return df
+
+
+if __name__ == "__main__":
+    # Test the function with your DataFrame
+    df = pd.read_csv('../uploads/sample_data.csv')
+    #print("Data types before inference:")
+    #print(df.dtypes)
+
+    df = infer_and_convert_data_types(df)
+
+   # print("\nData types after inference:")
+   # print(df.dtypes)
---
+++ dataCleaning/myapi/llm_api_client.py
@@ -0,0 +1,86 @@
+import os
+import google.generativeai as genai
+from IPython.display import Markdown
+import textwrap
+from api_keys import GOOGLE_API_KEY
+
+# run: 'gcloud auth application-default login', to login
+# You are in the llm-rag project
+# Service id: figure12-34@llm-rag-415122.iam.gserviceaccount.com
+
+class dataTypeLLM:
+    
+    def __init__(self):
+        self.setup_api()
+        self.model = genai.GenerativeModel('gemini-pro')
+        
+
+    def to_markdown(self, text):
+        text = text.replace('•', '  *')
+        return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+    def setup_api(self):
+        # Check if the environment variable exists
+        if 'GOOGLE_API_KEY' in os.environ:
+            print(f"The environment variable GOOGLE_API_KEY exists with value: {os.environ['GOOGLE_API_KEY']}")
+        else:
+            os.environ['GOOGLE_API_KEY'] = GOOGLE_API_KEY
+            print(f"Succesfully set the GOOGLE_API_KEY")
+
+        genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+
+    def prompt(self, input):
+        response = self.model.generate_content(input)
+        return response.parts
+    
+
+def llm_to_dtype(head):
+    queryDataTypeTemplate = f"""
+        Context: You are an expert Data Scientist that has been tasked with selecting the appropriate pandas
+        dtypes.
+
+        The pandas dtypes you can choose from are: [object, int64, int32, int6, int8, float64, float32, 
+        bool, datetime64, timedelta[ns], category, complex]
+
+        Please refer from using object as a dtype if possible.
+
+        You will take the first three rows of a pandas dataframe (data), inlcuding the headers, and determine the
+        appropriate dtype that I should use. Please only return the dtypes and NOTHING ELSE you suggest in the 
+        same order as they were read such that they correspond to the correct dataframe column. Refer to the example output.
+
+        Data: [{head}]
+
+        Example input:[ Data: [      Name  Birthdate Score Grade
+                                0    Alice  1/01/1990    90     A
+                                1      Bob  2/02/1991    75     B
+                                2  Charlie  3/03/1992    85     A]]
+        
+        Example Output: [object, datetime64, int32, category]
+    """
+
+    print(queryDataTypeTemplate)
+
+    gem = dataTypeLLM()
+
+    response = gem.prompt(queryDataTypeTemplate)
+    print(response)
+
+    print(80*'-')
+    def format_result(text):
+        """
+        Format text response from LLM into array of strings
+
+        Return:
+            - Array of strings
+        """
+        print(f'this is the text: {text}')
+        text = str(text).replace('[text: "[', '').replace(']"\n]', '')
+        text = text.replace(',', '')
+        entries = text.split()
+        print(entries) # TODO: Remove
+        print(len(entries)) # TODO: Remove
+
+        return entries
+
+    dtypes = format_result(response)
+    return dtypes
